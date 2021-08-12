ba3a76119673ee020149821f076acbf9fffd8518
Nhien Lam <nhienlam.ricky@gmail.com>
2021-08-11 21:24:54 -0700

    add skills and projects to nav bar

--- src/script.js
+++ src/script.js
@@ -26,22 +26,22 @@ function setTheme(mode)
 {
     if(mode == 'light')
     {
-        document.getElementById('theme-style').href = 'default.css'
+        document.getElementById('theme-style').href = 'styles/default.css'
     }
 
     if(mode == 'black')
     {
-        document.getElementById('theme-style').href = 'black.css'
+        document.getElementById('theme-style').href = 'styles/black.css'
     }
 
     if(mode == 'orange')
     {
-        document.getElementById('theme-style').href = 'orange.css'
+        document.getElementById('theme-style').href = 'styles/orange.css'
     }
 
     if(mode == 'blue')
     {
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = 'styles/blue.css'
     }
 
     localStorage.setItem('theme', mode)
