9e59be8483495fca7d598100f87e12175a427d9c
Nhien Lam <nhienlam.ricky@gmail.com>
2022-01-06 21:25:10 -0800

    updated info + checkingN (in progress)

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
-console.log("Its working")
+let theme = localStorage.getItem("theme");
 
-let theme = localStorage.getItem('theme')
-
-if(theme == null)
-{
-    setTheme('light')
-}
-else
-{
-    setTheme(theme)
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName("theme-dot");
 
-for(var i=0; themeDots.length > i; i++)
-{
-    themeDots[i].addEventListener('click', function(){
-        let mode = this.dataset.mode
-        console.log('Option clicked')
-        setTheme(mode)
-    })
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
 }
 
-function setTheme(mode)
-{
-    if(mode == 'light')
-    {
-        document.getElementById('theme-style').href = 'styles/default.css'
-    }
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "styles/default.css";
+  }
 
-    if(mode == 'black')
-    {
-        document.getElementById('theme-style').href = 'styles/black.css'
-    }
+  if (mode == "black") {
+    document.getElementById("theme-style").href = "styles/black.css";
+  }
 
-    if(mode == 'orange')
-    {
-        document.getElementById('theme-style').href = 'styles/orange.css'
-    }
+  if (mode == "orange") {
+    document.getElementById("theme-style").href = "styles/orange.css";
+  }
 
-    if(mode == 'mint')
-    {
-        document.getElementById('theme-style').href = 'styles/mint.css'
-    }
+  if (mode == "mint") {
+    document.getElementById("theme-style").href = "styles/mint.css";
+  }
 
-    localStorage.setItem('theme', mode)
+  localStorage.setItem("theme", mode);
 }
